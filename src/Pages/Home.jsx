d219ff7562bbc0cb4df6bf754a7a0071d2916d5e
teethw <kazisadman01@gmail.com>
2023-10-17 21:15:38 +0600

    Added about us section on home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
+import About from "../Components/About";
 import Banner from "../Components/Banner";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <About></About>
     </div>
   );
 };
